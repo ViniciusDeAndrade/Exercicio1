746a36cfc7ce20bd05c0891f4736003b12f54936
Marcos Vinicius de Araújo Andrade <viniciusclo@gmail.com>
2016-03-20 15:25:38 -0300

    Resolução TCP

--- src/cliente/Client.java
+++ src/cliente/Client.java
@@ -1,73 +1,72 @@
 package cliente;
-import java.io.BufferedReader;
+
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.InputStream;
 import java.net.Socket;
 
-import arquivo.Arquivo;
-
 public class Client {
-	public static void main(String[] args) throws IOException, 
-	ClassNotFoundException{
 
-		// cria socket e streams
-		Socket clientSocket = new Socket("localhost",1313);
-		ObjectOutputStream objetoParaOServidor = new ObjectOutputStream(clientSocket.getOutputStream());
-		ObjectInputStream objetoVindoDoServidor = new ObjectInputStream(clientSocket.getInputStream());
+	public static void main(String[] args) {
+		// Criando Classe cliente para receber arquivo
+		Client cliente = new Client();
 
-		//cria e instancia o arquivo
-		String localDoArquivo = "/home/vinicius/Área de Trabalho/arquivo-cliente.txt";
-		Arquivo a = new Arquivo(localDoArquivo);
-		File file = new File(a.getDiretorio());
-
-		try{
-			//efetivamente cria o arquivo
-			file.createNewFile();
-		}catch( IOException e){
-			e.printStackTrace();
-		}
-
-		// transforma em bytes 
-		FileInputStream arquivo = new FileInputStream(file);
-
-		//manda para o servidor
-		
-		byte [] buffer = new byte[4096];
-		while(true){
-			int len = arquivo.read(buffer);
-			if(len == -1)
-				break;
-			objetoParaOServidor.writeObject(arquivo);
-			objetoParaOServidor.flush();
-		}
-		
-		//recebe do servidor
-		String resposta = null;
-		resposta = (String) objetoVindoDoServidor.readObject();
-
-		System.out.println("---> " + resposta );
-
-		objetoParaOServidor.close();
-		objetoVindoDoServidor.close();
-		clientSocket.close();
-
-
-
-		/**byte [] buffer = new byte [4096];
-
-		while(true){
-			int len = arquivo.read(buffer);
-			if(len == -1)
-				break;
-			objetoParaOServidor.write(buffer,0,len);
-			objetoParaOServidor.flush();
-		}
-		 **/
+		// Solicitando arquivo
+		cliente.getArquidoDoServidor();
 	}
 
+	private void getArquidoDoServidor() {
+		Socket socket = null;
+		FileOutputStream fileOutputStream = null;
+		InputStream inputStream = null;
+
+		try {
+			// Criando conexão com o servidor
+			System.out.println("Conectando ao Servidor na 13267");
+			socket = new Socket("127.0.0.1", 13267);
+			inputStream = socket.getInputStream();
+
+			// Cria arquivo local no cliente
+			fileOutputStream = new FileOutputStream(new File("/home/vinicius/Área de Trabalho/arquivo-cliente.txt"));
+			System.out.println("Arquivo Criado /home/vinicius/Área de Trabalho/arquivo-cliente.txt");
+
+			// Prepara variaveis para transferencia
+			byte[] buffer = new byte[1024];
+			int bytesLidos;
+
+			// Copia conteudo do canal
+			System.out.println("Recebendo Arquivo...");
+			while ((bytesLidos = inputStream.read(buffer)) != -1) {
+				fileOutputStream.write(buffer, 0, bytesLidos);
+				fileOutputStream.flush();
+			}
+			System.out.println("Arquivo Recebido!");
+		} catch (Exception exception) {
+			// Mostra erro no console
+			exception.printStackTrace();
+		} finally {
+			if (socket != null) {
+				try {
+					socket.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
+			if (fileOutputStream != null) {
+				try {
+					fileOutputStream.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- src/principal/Server.java
+++ src/principal/Server.java
@@ -1,74 +1,82 @@
 package principal;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
-import arquivo.Arquivo;
-
-import java.net.ServerSocket;
-
-//classe arquivo
-
 public class Server {
-	public static void main(String[] args)throws IOException {
+	
+	public static void main(String[] args) {
+		// Criando servidor
+		Server server = new Server();
 
+		// Aguardar conexao de cliente para transferia
+		server.esperaCliente();
+	}
 
-		ServerSocket welcomeSocket = new ServerSocket(1313);
-		System.out.println("Servidor on!");
-		Socket socket = welcomeSocket.accept();
-		ObjectInputStream objetoVindoDoCliente = new ObjectInputStream(socket.getInputStream());
-		ObjectOutputStream objetoQueVaiProCliente = new ObjectOutputStream(socket.getOutputStream());
-		
-		
-		
-		//FileOutputStream arquivo = new FileOutputStream("/home/vinicius/Área de Trabalho/arquivo-cliente.txt");
-		//receba do cliente
-		FileInputStream arquivoEmBytes = null;
+	public void esperaCliente() {
+		// Checa se a transferencia foi completada com sucesso
+		OutputStream outputStream = null;
+		ServerSocket serverSocket = null;
+		FileInputStream fileInputStream = null;
+
 		try {
-			arquivoEmBytes = (FileInputStream) objetoVindoDoCliente.readObject();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Abrindo porta para conexao de clients
+			serverSocket = new ServerSocket(13267);
+			System.out.println("Porta de conexao aberta 13267");
+
+			// Cliente conectado
+			Socket socket = serverSocket.accept();
+			System.out.println("Conexao recebida pelo cliente");
+
+			// Criando tamanho de leitura
+			byte[] buffer = new byte[1024];
+			int bytesLidos;
+
+			// Criando arquivo que sera transferido pelo servidor
+			File file = new File("/home/vinicius/Área de Trabalho/arquivo-servidor.txt");
+			fileInputStream = new FileInputStream(file);
+			System.out.println("Lendo arquivo /home/vinicius/Área de Trabalho/arquivo-servidor.txt");
+			
+			
+			// Criando canal de transferencia
+			outputStream = socket.getOutputStream();
+
+			// Lendo arquivo criado e enviado para o canal de transferencia
+			System.out.println("Enviando Arquivo...");
+			while ((bytesLidos = fileInputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesLidos);
+				outputStream.flush();
+			}
+			System.out.println("Arquivo Enviado!");
+		} catch (Exception exception) {
+			// Mostra erro no console
+			exception.printStackTrace();
+		} finally {
+			if (outputStream != null) {
+				try {
+					outputStream.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
+			if (serverSocket != null) {
+				try {
+					serverSocket.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
+			if (fileInputStream != null) {
+				try {
+					fileInputStream.close();
+				} catch (IOException iOException) {
+					iOException.printStackTrace();
+				}
+			}
 		}
-		
-		
-		//processamento da mensagem
-		byte []buf = new byte[4096];
-		while(true){
-			int len = arquivoEmBytes.read(buf);
-			if(len==-1)
-				break;
-			objetoQueVaiProCliente.write(buf,0,len);
-		}		
-		
-		//responde pro cliente
-				
-		
-		objetoQueVaiProCliente.close();
-		welcomeSocket.close();
-		socket.close();
-
-
-
-		/**
-		while(true){
-			int len = objetoSerializado.read(buf);
-			if(len == -1 )
-				break;
-			arquivo.write(buf,0, len);
-		}
-		 **/
-
-
-
-	}	
-
-
+	}
 }
